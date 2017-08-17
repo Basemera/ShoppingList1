651e4e6deeab7e02c4e60a4f31889554c1c9e580
Basemera Phiona <bsmrrachel@gmail.com>
2017-08-17 19:26:39 +0300

    adding background color

--- app/update.py
+++ app/update.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, app
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/update' methods = "POST")
+@app.route('/update' methods=['GET', 'POST'])
 def deleteitem():
     if request.method == 'POST':
         del dict[key]
